280e110404e27c4309a6b5d4e1ec381d4ecdf170
jbach65 <joshbsteinbach@gmail.com>
2019-12-04 12:33:36 -0500

    added Win-Stay approach

--- winStay.py
+++ winStay.py
@@ -2,13 +2,13 @@ from slotMachine import slotMachine
 from random import random
 
 def main():
-	print("Upper Confidence Bound Solution")
+	print("Win-Stay Solution")
 	#initialize slot machines
 	slotList = []
-	for i in range(10):
+	for i in range(2):
 		slotList.append(slotMachine(i))
-	#slotList[0].winRate = .1
-	#slotList[1].winRate = .09
+	slotList[0].winRate = .2
+	slotList[1].winRate = .4
 
 	print("generated {} slotMachines:".format(len(slotList)))
 	for obj in slotList:
@@ -17,24 +17,26 @@ def main():
 	inp = input("Press enter to continue, i + enter for current slot info, or q + enter to quit: ")
 	leverPulls = 0
 	totalWins = 0
+	currentBandit = 0
 	while inp != "q" and inp != "Q":
 		if inp == "i" or inp == "I":
 			for obj in slotList:
-				print("slotMachine {} (wins: {}, losses: {}, UCB: {})\n\tactual win rate:\t{}".format(obj.id, obj.wins, obj.losses, obj.upperConfidenceBound, obj.winRate))
+				print("slotMachine {} (wins: {}, losses: {})\n\tactual win rate:\t{}".format(obj.id, obj.wins, obj.losses, obj.winRate))
 			if(leverPulls !=0):
 				print("Total Wins: {}, Total Pulls: {}, Total Win Rate: {}".format(totalWins, leverPulls,totalWins/leverPulls))
 		elif inp == "":
 			win = "no"
-			slotList.sort(key=lambda x: x.upperConfidenceBound, reverse=True)	#sort by upper confidence bound
-			print("slotMachine {} was selected (wins: {}, losses: {}, UCB: {})\n\tactual win rate:\t{}".format(slotList[0].id, slotList[0].wins, slotList[0].losses, slotList[0].upperConfidenceBound, slotList[0].winRate))
-			if random() < slotList[0].winRate:
-				slotList[0].wins += 1
+			print("slotMachine {} was selected (wins: {}, losses: {})\tactual win rate:\t{}".format(slotList[currentBandit].id, slotList[currentBandit].wins, slotList[currentBandit].losses, slotList[currentBandit].winRate))
+			if random() < slotList[currentBandit].winRate:
+				slotList[currentBandit].wins += 1
 				totalWins += 1
 				win = "yes"
 			else:
-				slotList[0].losses += 1
+				slotList[currentBandit].losses += 1
+				currentBandit += 1
+				if currentBandit == len(slotList):
+					currentBandit = 0
 			leverPulls+=1
-			slotList[0].calculateUCB()
 			print("\tWin: {}\n\tTotal Wins: {}, Total Pulls: {}, Total Win Rate: {}".format(win, totalWins, leverPulls,totalWins/leverPulls))
 		else:
 			print("Does not compute")
